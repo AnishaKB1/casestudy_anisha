c41f0e5497d71be52a88661bc0c46768a48912d6
AnishaKB1 <anishakb4@gmail.com>
2023-08-05 13:17:02 +0530

    updation in prime

--- Prime.js
+++ Prime.js
@@ -1,6 +1,6 @@
 //Write a JavaScript function to get an array and to read the first element of an array. Check whether it is prime or not. 
 var NumArray=[];
-//function for gettina an array
+
 function CreateArray(length) {
    
    for (var i=2 ;i<=length;i++) {
